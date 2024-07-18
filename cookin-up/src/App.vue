0b9d71b3e20475314112452130cd7ab9307a0d3d
Marcos Nobre <marconobre@gmail.com>
2024-07-17 21:19:37 -0300

    Atualização

---
+++ cookin-up/src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Banner from './components/Banner.vue';
+  export default {
+    components: { Banner: Banner }
+  }
+</script>
+
+<template>
+  <Banner />
+</template>
+
+<style scoped>
+
+</style>

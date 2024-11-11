d685c881a3acd12ddabbee2eafbbc12e502719d3
marcosanobre <marconobre@gmail.com>
2024-11-11 16:36:24 -0300

    MNOBRE: Atualização

---
+++ cookin-up/src/components/ConteudoPrincipal.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export {};
+</script>
+
+<template>
+    <main class="conteudo-principal">
+        <section>
+            <span class="subtitulo-lg sua-lista-texto">
+                Sua lista:
+            </span>
+
+            ul.ingredientes-sua-lista
+        </section>
+    </main>
+
+</template>
